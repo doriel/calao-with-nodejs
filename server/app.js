bdcae2dcca0c872807a5ef72d6df6dfc601d584c
Firmino Changani <flowck96@gmail.com>
2018-02-28 16:42:17 +0100

    Set up the database connection and require the models

--- server/app.js
+++ server/app.js
@@ -1,8 +1,29 @@
 // Dependencies
 const express = require('express');
+const Sequelize = require('sequelize');
 
 // Init express
 const app = express();
+
+// Init the db
+const sequelize = new Sequelize('mysql://root:tosh@localhost/slangs_angola')
+sequelize.authenticate()
+.then(() => { console.log('DB is connected'); })
+.catch((e) => { console.log(`DB is not connected: ${e}`);} )
+
+// Models
+const Admin = require('./models/Admin')(sequelize);
+const User = require('./models/User')(sequelize);
+
+app.get('/', (req, res) => {
+
+	User.findAll()
+	.then((admins) => {
+		console.log(admins)
+	});
+
+	res.send('OK');
+});
 
 // Listen express application to server port
 let port = 3000;
